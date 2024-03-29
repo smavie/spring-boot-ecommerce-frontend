abd712fa1a1e0507c3a6f588089bc50b06c8b357
Sergio Mavie <sergio.mavie@gmail.com>
2024-03-29 13:22:23 +0200

    Product class and Product Service created - Part 2

--- src/app/common/product.ts
+++ src/app/common/product.ts
@@ -10,6 +10,5 @@ export class Product {
         public unitsInStock: number,
         public dateCreated: Date,
         public lastUpdated: Date
-
     ) {}
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { Product } from '../common/product';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+
+  private baseUrl = 'http://localhost:8080/api/products';
+
+
+  constructor(private httpClent: HttpClient) { }
+  productList(): Observable<Product[]>{
+    return this.httpClent.get<GetResponse> (this.baseUrl).pipe(map(response => response._embedded.products));
+  }
+
+}
+
+interface GetResponse{
+  _embedded: {
+    products: Product[];
+  }
 }
