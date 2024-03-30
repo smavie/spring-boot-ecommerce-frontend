cc4c42e23b1c869ba50282bed938f41d7480c2fc
Sergio Mavie <sergio.mavie@gmail.com>
2024-03-30 11:57:05 +0200

    Angular Project - Dynamic Search Component - Call Product Service

--- src/app/components/product-category-menu/product-category-menu.component.ts
+++ src/app/components/product-category-menu/product-category-menu.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ProductCategory } from '../../common/product-category';
+import { ProductService } from '../../services/product.service';
 
 @Component({
   selector: 'app-product-category-menu',
   templateUrl: './product-category-menu.component.html',
   styleUrl: './product-category-menu.component.css'
 })
-export class ProductCategoryMenuComponent {
+export class ProductCategoryMenuComponent implements OnInit {
+
+  productCategories: ProductCategory[] = [];
+
+  constructor(private productService: ProductService){}
+  ngOnInit(): void {
+    this.ListProductCategories();
+  }
+  
+  ListProductCategories() {
+    this.productService.getProductCategories().subscribe(
+      data => {
+        console.log('Product categories =' + JSON.stringify(data));
+        this.productCategories = data;
+      }
+    )
+  }
 
 }
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, map } from 'rxjs';
 import { Product } from '../common/product';
+import { ProductCategory } from '../common/product-category';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,34 @@ import { Product } from '../common/product';
 export class ProductService {
 
 
+
   private baseUrl = 'http://localhost:8080/api/products';
+  private categoyUrl = 'http://localhost:8080/api/product-category';
 
 
   constructor(private httpClent: HttpClient) { }
 
   getProductList(theCategoryId: number): Observable<Product[]>{
     const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
-    return this.httpClent.get<GetResponse> (searchUrl).pipe(map(response => response._embedded.products));
+    return this.httpClent.get<GetResponseProduct> (searchUrl).pipe(
+      map(response => response._embedded.products));
+  }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClent.get<GetResponseProductCategory> (this.categoyUrl).pipe(
+      map(response => response._embedded.productCategories));
   }
 
 }
 
-interface GetResponse{
+interface GetResponseProduct{
   _embedded: {
     products: Product[];
+  }
+
+}
+interface GetResponseProductCategory{
+  _embedded: {
+    productCategories: ProductCategory[];
   }
 }
